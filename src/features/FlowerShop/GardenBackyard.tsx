949c1252a84c490779b9b9604d6e2019f2ba8c4f
Rokcha_latte <ojr5521@naver.com>
2025-10-20 22:45:41 +0900

    체인소맨 등장

--- src/features/FlowerShop/GardenBackyard.tsx
+++ src/features/FlowerShop/GardenBackyard.tsx
@@ -4,7 +4,7 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+
 import {
   Dialog,
   DialogContent,
